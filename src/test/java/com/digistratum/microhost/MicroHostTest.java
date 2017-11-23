41ff5c23d1cb1a38b6f89729d5569cfc88c62457
skelly <keenskelly@gmail.com>
2017-11-22 18:41:40 -0800

    Set up MicroHost test for some mocks and such
    WIP

--- src/test/java/com/digistratum/microhost/MicroHostTest.java
+++ src/test/java/com/digistratum/microhost/MicroHostTest.java
@@ -1,7 +1,37 @@
 package com.digistratum.microhost;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.digistratum.microhost.Database.Mysql.MySqlConnectionPool;
+import com.digistratum.microhost.Database.Mysql.MySqlConnectionPoolFactory;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.mockito.Mockito.mock;
 
 class MicroHostTest {
+
+
+	MHConfigFactory mockMHConfigFactory;
+	MySqlConnectionPoolFactory mockMySqlConnectionPoolFactory;
+	ServerFactory mockServerFactory;
+
+	MySqlConnectionPool mockMySqlConnectionPool;
+
+	@BeforeAll
+	public void setup() {
+		mockMHConfigFactory = mock(MHConfigFactory .class);
+		mockMySqlConnectionPoolFactory = mock(MySqlConnectionPoolFactory.class);
+		mockServerFactory = mock(ServerFactory.class);
+		
+		mockMySqlConnectionPool = mock(MySqlConnectionPool.class);
+
+	}
+
+	public void testThatRunDoesSomething() {
+
+	}
+
+	private class TestableMicroHost extends MicroHost {
+		@Override
+		public void run(MHConfigFactory mhcf, MySqlConnectionPoolFactory mscpf, ServerFactory sf) {}
+	}
 
 }
