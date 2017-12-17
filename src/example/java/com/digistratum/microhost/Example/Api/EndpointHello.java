522615c329a23bbd6ff70c1ac80fc278d6bc9572
skelly <keenskelly@gmail.com>
2017-12-17 15:04:18 -0800

    Convert Hello endpoint to use EndpointImpl
    Also add Content-Type headers for both JSON and TEXT

--- src/example/java/com/digistratum/microhost/Example/Api/EndpointHello.java
+++ src/example/java/com/digistratum/microhost/Example/Api/EndpointHello.java
@@ -1,16 +1,13 @@
 package com.digistratum.microhost.Example.Api;
 
-import com.digistratum.microhost.RestServer.Endpoint.Endpoint;
 import com.digistratum.microhost.Exception.MHException;
+import com.digistratum.microhost.RestServer.Endpoint.EndpointImpl;
 import com.digistratum.microhost.RestServer.Http.RequestResponse.Request;
 import com.digistratum.microhost.RestServer.Http.RequestResponse.Response;
-import com.digistratum.microhost.RestServer.Http.RequestResponse.ResponseImpl;
 
-public class EndpointHello implements Endpoint {
+public class EndpointHello extends EndpointImpl {
+	@Override
 	public Response handle(Request request) throws MHException {
-		return new ResponseImpl(
-			200,
-			"Hello, World!"
-		);
+		return textResponse200("Hello, World!");
 	}
 }
--- src/main/java/com/digistratum/microhost/RestServer/Endpoint/EndpointImpl.java
+++ src/main/java/com/digistratum/microhost/RestServer/Endpoint/EndpointImpl.java
@@ -1,6 +1,8 @@
 package com.digistratum.microhost.RestServer.Endpoint;
 
 import com.digistratum.microhost.Exception.MHException;
+import com.digistratum.microhost.RestServer.Http.Headers.Headers;
+import com.digistratum.microhost.RestServer.Http.Headers.HeadersImpl;
 import com.digistratum.microhost.RestServer.Http.RequestResponse.Request;
 import com.digistratum.microhost.RestServer.Http.RequestResponse.Response;
 import com.digistratum.microhost.RestServer.Http.RequestResponse.ResponseImpl;
@@ -24,11 +26,41 @@ abstract public class EndpointImpl implements Endpoint {
 		return null;
 	}
 
-	public Response jsonResponse200(Object data) throws MHException {
+	/**
+	 * Provide a JSON data response with 200 OK status code
+	 *
+	 * @param data Object that we want to JSON-encode for the response
+	 *
+	 * @return Response instance populated with the data and status code
+	 *
+	 * @throws MHException for any errors
+	 */
+	protected Response jsonResponse200(Object data) throws MHException {
+		Headers responseHeaders = new HeadersImpl();
+		responseHeaders.set("Content-Type", "application/json");
 		String responseBody = gson.toJson(data);
 		return new ResponseImpl(
 				200,
+				responseHeaders,
 				responseBody
+		);
+	}
+
+	/**
+	 * Provide a TEXT response with 200 OK status code
+	 *
+	 * @param text String that we want to return for the response
+	 *
+	 * @return Response instance populated with the text and status code
+	 *
+	 * @throws MHException for any errors
+	 */
+	protected Response textResponse200(String text) throws MHException {
+		Headers responseHeaders = new HeadersImpl();
+		responseHeaders.set("Content-Type", "text/plain");
+		return new ResponseImpl(
+				200,
+				text
 		);
 	}
 }
