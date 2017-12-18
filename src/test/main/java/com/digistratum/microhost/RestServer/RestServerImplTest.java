133a3765f9c85edee1709fc2a03a6675eec16450
skelly <keenskelly@gmail.com>
2017-12-17 22:57:40 -0800

    Complete test coverage of RestServerImpl.addControllerContext

--- src/test/main/java/com/digistratum/microhost/RestServer/RestServerImplTest.java
+++ src/test/main/java/com/digistratum/microhost/RestServer/RestServerImplTest.java
@@ -8,36 +8,90 @@ import com.sun.net.httpserver.HttpServer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class RestServerImplTest {
 	private RestServerImpl sut;
 	private Config mockConfig;
 	private HttpServerFactory mockHttpServerFactory;
-
+	private HttpServer mockHttpServer;
 
 	@BeforeEach
 	public void setup() throws Exception {
 		mockConfig = mock(Config.class);
 		doReturn(1).when(mockConfig).get(anyString(), anyInt());
 		mockHttpServerFactory = mock(HttpServerFactory.class);
-		doReturn(mock(HttpServer.class)).when(mockHttpServerFactory).getInstance(anyInt());
+		mockHttpServer = mock(HttpServer.class);
+		doReturn(mockHttpServer).when(mockHttpServerFactory).getInstance(anyInt());
 		sut = new RestServerImpl(mockConfig, mockHttpServerFactory);
 	}
 
 	@Test
-	public void testThat_addControllerContext_rejectsNullContext() {
+	public void testThat_addControllerContext_rejectsNullController() {
 		assertThrows(
-			MHException.class,
-			()-> {
-				Controller mockCtrl = mock(Controller.class);
-				sut.addControllerContext(mockCtrl, null);
-			}
+				MHException.class,
+				()-> {
+					sut.addControllerContext(null, "imaginary context");
+				}
 		);
+	}
+
+	@Test
+	public void testThat_addControllerContext_rejectsNullContext() {
+		Controller mockCtrl = mock(Controller.class);
+		assertThrows(
+				MHException.class,
+				()-> {
+					sut.addControllerContext(mockCtrl, null);
+				}
+		);
+	}
+
+	@Test
+	public void testThat_addControllerContext_rejectsEmptyContext() {
+		Controller mockCtrl = mock(Controller.class);
+		assertThrows(
+				MHException.class,
+				()-> {
+					sut.addControllerContext(mockCtrl, "");
+				}
+		);
+	}
+
+	@Test
+	public void testThat_addControllerContext_rejectsDuplicateContext() {
+		String context = "/testContext";
+		Controller mockCtrl = mock(Controller.class);
+		try {
+			sut.addControllerContext(mockCtrl, context);
+		} catch (MHException e) {
+			// Shouldn't happen, but just in case...
+			assertTrue(false);
+		}
+		assertThrows(
+				MHException.class,
+				() -> {
+					sut.addControllerContext(mockCtrl, context);
+				}
+		);
+	}
+
+	@Test
+	public void testThat_addControllerContext_addsContextToServer() {
+		String context = "/testContext";
+		Controller mockCtrl = mock(Controller.class);
+		try {
+			sut.addControllerContext(mockCtrl, context);
+		} catch (MHException e) {
+			// Shouldn't happen, but just in case...
+			assertTrue(false);
+		}
+		verify(mockHttpServer, times(1)).createContext(anyString(), anyObject());
 	}
 }
