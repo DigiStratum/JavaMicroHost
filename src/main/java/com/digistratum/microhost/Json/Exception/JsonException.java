7592172ddb8c0569c4d56275d4e45b14b48fa8f9
skelly <keenskelly@gmail.com>
2018-01-23 01:06:08 -0800

    Adds JsonException RuntimeException
    This is so that we can passively report certain runtime errors while attempting to serialize JSON

---
+++ src/main/java/com/digistratum/microhost/Json/Exception/JsonException.java
@@ -0,0 +1,10 @@
+package com.digistratum.microhost.Json.Exception;
+
+public class JsonException extends RuntimeException {
+	public JsonException(String message) { super(message); }
+	public JsonException(String message, Throwable cause) { super(message, cause); }
+	public JsonException(Throwable cause) { super(cause); }
+	public JsonException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
