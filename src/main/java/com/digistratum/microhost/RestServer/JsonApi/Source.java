16ae92f028d4ab896bef5e2128c5004f15ebf7ca
skelly <keenskelly@gmail.com>
2018-01-23 00:31:33 -0800

    Convert JsonApi.Source to strict model
    Adds helper methods to set the properties and test coverage.

--- src/main/java/com/digistratum/microhost/RestServer/JsonApi/Source.java
+++ src/main/java/com/digistratum/microhost/RestServer/JsonApi/Source.java
@@ -1,6 +1,67 @@
 package com.digistratum.microhost.RestServer.JsonApi;
 
-public class Source {
-	public JsonPointer pointer;
-	public String parameter;
+import com.digistratum.microhost.Json.JsonBuilder;
+import com.digistratum.microhost.Json.JsonClass;
+
+/**
+ * JsonApi - Source
+ *
+ * An object containing references to the source of the error, should one occur.
+ */
+public class Source implements JsonClass {
+	protected Properties properties;
+	protected JsonBuilder jsonBuilder;
+
+	/**
+	 * Our properties data structure
+	 */
+	protected class Properties {
+		/**
+		 * A JSON Pointer [RFC6901] to the associated entity in the request document [e.g. "/data"
+		 * for a primary data object, or "/data/attributes/title" for a specific attribute].
+		 */
+		public JsonPointer pointer;
+
+		/**
+		 * A string indicating which URI query parameter caused the error.
+		 */
+		public String parameter;
+	}
+
+	/**
+	 * Constructor
+	 */
+	public Source () {
+		properties = new Properties();
+	}
+
+	/**
+	 * Setter for Pointer
+	 *
+	 * @param pointer JsonPointer instance to set against our properties
+	 *
+	 * @return This instance for chaining...
+	 */
+	public Source setPointer(JsonPointer pointer) {
+		properties.pointer = pointer;
+		return this;
+	}
+
+	/**
+	 * Setter for Parameter
+	 *
+	 * @param parameter String to set against our parameter
+	 *
+	 * @return This instance for chaining...
+	 */
+	public Source setParameter(String parameter) {
+		properties.parameter = parameter;
+		return this;
+	}
+
+	@Override
+	public String toJson() {
+		if (null == jsonBuilder) jsonBuilder = new JsonBuilder();
+		return jsonBuilder.toJson(properties);
+	}
 }
---
+++ src/test/main/java/com/digistratum/microhost/RestServer/JsonApi/SourceTest.java
@@ -0,0 +1,27 @@
+package com.digistratum.microhost.RestServer.JsonApi;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SourceTest {
+	private Source sut;
+
+	@BeforeEach
+	public void setup() throws Exception {
+		sut = new Source();
+	}
+
+	@Test
+	public void testThat_toJson_returnsBareObjectForBareSource() {
+		assertEquals("{}", sut.toJson());
+	}
+
+	@Test
+	public void testThat_toJson_returnsLoadedObjectForLoadedSource() {
+		JsonPointer jsonPointer = new JsonPointer("yoh");
+		sut.setParameter("parameterName").setPointer(jsonPointer);
+		assertEquals("{\"pointer\":\"yoh\",\"parameter\":\"parameterName\"}", sut.toJson());
+	}
+}
