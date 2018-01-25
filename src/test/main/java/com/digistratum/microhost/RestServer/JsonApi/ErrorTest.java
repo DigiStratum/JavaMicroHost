c224cbaa8a9c5cec418eb3b4ae4dd033777f689a
skelly <keenskelly@gmail.com>
2018-01-24 21:49:09 -0800

    Complete test coverage for JsonApi.Errors class

--- src/test/main/java/com/digistratum/microhost/RestServer/JsonApi/ErrorTest.java
+++ src/test/main/java/com/digistratum/microhost/RestServer/JsonApi/ErrorTest.java
@@ -19,5 +19,21 @@ public class ErrorTest {
 		assertEquals("{}", res);
 	}
 
-	// TODO: Fill in additional test cases covering the various, chained setters
+	@Test
+	public void testThat_setters_chainPropertly() {
+		Meta meta = new Meta();
+		Source source = new Source();
+		Links links = new Links();
+		sut.setId("id")
+				.setCode("CODE")
+				.setDetail("DETAIL")
+				.setStatus("STATUS")
+				.setTitle("TITLE")
+				.setMeta(meta)
+				.setSource(source)
+				.setLinks(links)
+				.setId("ID");
+		String res = sut.toJson();
+		assertEquals("{\"id\":\"ID\",\"links\":{},\"status\":\"STATUS\",\"code\":\"CODE\",\"title\":\"TITLE\",\"detail\":\"DETAIL\",\"source\":{},\"meta\":{}}", res);
+	}
 }
