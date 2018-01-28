81314f09efced90da43a2b5f6d7d0a5b5969ca30
skelly <keenskelly@gmail.com>
2018-01-28 10:04:22 -0800

    Update dependencies and remove old test coverage
    Tests were for JsonApj, JsonValidation, etc which are now separate projects

--- src/main/java/com/digistratum/microhost/RestServer/JsonApi/DynamicClass/Attributes.java
+++
@@ -1,5 +0,0 @@
-package com.digistratum.microhost.RestServer.JsonApi.DynamicClass;
-
-// TODO: Add methods specific to attributes
-public class Attributes extends DynamicClass {
-}
--- src/main/java/com/digistratum/microhost/RestServer/JsonApi/DynamicClass/Meta.java
+++
@@ -1,5 +0,0 @@
-package com.digistratum.microhost.RestServer.JsonApi.DynamicClass;
-
-// TODO: Add functions which are specific to Metadata
-public class Meta extends DynamicClass {
-}
